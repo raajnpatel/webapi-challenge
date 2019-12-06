95600ac85fafdf24951dd4602d5f9df237158c00
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-06 09:34:19 -0800

    actionRouter complete (very similar to projectRouter)

--- data/actions/actionRouter.js
+++ data/actions/actionRouter.js
@@ -34,6 +34,74 @@ router.get('/:id', validateActionId, (req, res) => {
         })
 });
 
+router.post('/', (req, res) => {
+    const text = req.body;
+    Action.insert(text)
+        .then(text => {
+            console.log(text);
+            res
+                .status(201)
+                .json(text)
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"There was a problem reaching the server."})
+        })
+});
+
+router.put('/:id', validateActionId, (req, res) => {
+    const { id } = req.params;
+    const text = req.body;
+    Action.update(id, text)
+        .then(updated => {
+            if(updated){
+                console.log(updated);
+                Action.get(id)
+                    .then(newText => {
+                        console.log(newText);
+                        if(newText){
+                            res
+                                .status(201)
+                                .json(newText)
+                        } else {
+                            res
+                                .status(404)
+                                .json({error:"Error making changes."})
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        res
+                            .status(500)
+                            .json({error:"Error reaching server."})
+                    })
+            }
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Action.remove(id)
+        .then(removed => {
+            if(removed) {
+                res
+                    .status(204)
+                    .json({message: "The entry has been deleted."})
+            } else {
+                res
+                    .status(404)
+                    .json({error:"That entry ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"There was an error reaching the server."})
+        })
+});
 
 function validateActionId(req, res, next) {
     const { id } = req.params;
